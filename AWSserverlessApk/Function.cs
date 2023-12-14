625069a91d0f40a58f568e074eebb3a1a31825d1
Aagam Shah <shah.aagam126@gmail.com>
2023-12-14 06:19:27 -0500

    thumbnail done

--- AWSserverlessApk/Function.cs
+++ AWSserverlessApk/Function.cs
@@ -2,91 +2,92 @@ using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
-
-
 using Amazon.Rekognition;
 using Amazon.Rekognition.Model;
-
 using Amazon.S3;
-using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace AWSserverlessApk;
-
-public class Function
+namespace AWSserverlessApk
 {
-    private readonly IAmazonRekognition _rekognitionClient;
-    private readonly IAmazonDynamoDB _dynamoDbClient;
-
-    public Function()
+    public class Function
     {
-        _rekognitionClient = new AmazonRekognitionClient();
-        _dynamoDbClient = new AmazonDynamoDBClient();
-    }
+        private readonly IAmazonRekognition _rekognitionClient;
+        private readonly IAmazonDynamoDB _dynamoDbClient;
 
-    public async Task FunctionHandler(S3Event s3Event, ILambdaContext context)
-    {
-        var s3ObjectKey = s3Event.Records[0].S3.Object.Key;
-        var imageUrl = $"s3://{s3Event.Records[0].S3.Bucket.Name}/{s3ObjectKey}";
-
-        // Use Rekognition service to detect labels
-        var detectedLabels = await DetectLabelsAsync(imageUrl);
-
-        // Filter labels with confidence > 90
-        var highConfidenceLabels = detectedLabels.Labels
-            .Where(label => label.Confidence > 90)
-            .Select(label => new { Name = label.Name, Confidence = label.Confidence })
-            .ToList();
-
-        // Construct DynamoDB item with image URL and high-confidence labels
-        var item = new Document();
-        item["ImageID"] = Guid.NewGuid().ToString();
-        item["ImageURL"] = imageUrl;
-
-        // Create a DynamoDBList to hold label information
-        var labelList = highConfidenceLabels
-            .Select(label => new DynamoDBEntry[]
-            {
-            new Primitive(label.Name),
-            new Primitive(label.Confidence.ToString())
-            })
-            .SelectMany(x => x)
-            .ToList();
-
-        // Assign the labelList to the Labels attribute in the Document
-        item["Labels"] = new DynamoDBList(labelList);
-
-        // Insert item into DynamoDB table
-        await InsertItemToDynamoDBAsync(item);
-    }
-
-
-    private async Task<DetectLabelsResponse> DetectLabelsAsync(string imageUrl)
-    {
-        var request = new DetectLabelsRequest
+        public Function()
         {
-            Image = new Image
+            _rekognitionClient = new AmazonRekognitionClient();
+            _dynamoDbClient = new AmazonDynamoDBClient();
+        }
+
+        public async Task FunctionHandler(S3Event s3Event, ILambdaContext context)
+        {
+            foreach (var record in s3Event.Records)
             {
-                S3Object = new Amazon.Rekognition.Model.S3Object
+                var s3ObjectKey = record.S3.Object.Key;
+                var imageUrl = $"s3://{record.S3.Bucket.Name}/{s3ObjectKey}";
+
+                // Use Rekognition service to detect labels
+                var detectedLabels = await DetectLabelsAsync(imageUrl);
+
+                // Filter labels with confidence > 90
+                var highConfidenceLabels = detectedLabels.Labels
+                    .Where(label => label.Confidence > 90)
+                    .Select(label => new { Name = label.Name, Confidence = label.Confidence })
+                    .ToList();
+
+                // Construct DynamoDB item with image URL and high-confidence labels
+                var item = new Document();
+                item["ImageID"] = Guid.NewGuid().ToString();
+                item["ImageURL"] = imageUrl;
+
+                // Create a DynamoDBList to hold label information
+                var labelList = highConfidenceLabels
+                    .Select(label => new DynamoDBEntry[]
+                    {
+                        new Primitive(label.Name),
+                        new Primitive(label.Confidence.ToString())
+                    })
+                    .SelectMany(x => x)
+                    .ToList();
+
+                // Assign the labelList to the Labels attribute in the Document
+                item["Labels"] = new DynamoDBList(labelList);
+
+                // Insert item into DynamoDB table
+                await InsertItemToDynamoDBAsync(item);
+            }
+        }
+
+        private async Task<DetectLabelsResponse> DetectLabelsAsync(string imageUrl)
+        {
+            var request = new DetectLabelsRequest
+            {
+                Image = new Image
                 {
-                    Bucket = "images-detection",
-                    Name = "Editor_2_20220524215249.jpg"
-                }
-            },
-            MinConfidence = 90F
-        };
+                    S3Object = new Amazon.Rekognition.Model.S3Object
+                    {
+                        Bucket = "images-detection", // Replace with your S3 bucket name
+                        Name = imageUrl.Substring(imageUrl.IndexOf("images-detection") + "images-detection/".Length)
+                    }
+                },
+                MinConfidence = 90F
+            };
 
-        var response = await _rekognitionClient.DetectLabelsAsync(request);
-        return response;
-    }
+            var response = await _rekognitionClient.DetectLabelsAsync(request);
+            return response;
+        }
 
-    private async Task InsertItemToDynamoDBAsync(Document item)
-    {
-        Table table = Table.LoadTable(_dynamoDbClient, "ImageLabelsTable");
+        private async Task InsertItemToDynamoDBAsync(Document item)
+        {
+            Table table = Table.LoadTable(_dynamoDbClient, "ImageLabelsTable");
 
-        await table.PutItemAsync(item);
+            await table.PutItemAsync(item);
+        }
     }
 }
--- ImagesToThumbnail/Function.cs
+++ ImagesToThumbnail/Function.cs
@@ -2,92 +2,77 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using Amazon.S3.Model;
-using Amazon.S3.Util;
+using GrapeCity.Documents.Imaging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace ImagesToThumbnail;
-
-public class Function
+namespace ImagesToThumbnail
 {
-    IAmazonS3 S3Client { get; set; }
-
-    /// <summary>
-    /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
-    /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
-    /// region the Lambda function is executed in.
-    /// </summary>
-    public Function()
+    public class Function
     {
-        S3Client = new AmazonS3Client();
-    }
+        private const string DestinationBucketName = "imaging-destination"; // Replace with your destination bucket name
+        private readonly IAmazonS3 _s3Client = new AmazonS3Client();
 
-    /// <summary>
-    /// Constructs an instance with a preconfigured S3 client. This can be used for testing outside of the Lambda environment.
-    /// </summary>
-    /// <param name="s3Client"></param>
-    public Function(IAmazonS3 s3Client)
-    {
-        this.S3Client = s3Client;
-    }
-
-    /// <summary>
-    /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
-    /// to respond to S3 notifications.
-    /// </summary>
-    /// <param name="evnt"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public async Task<string> FunctionHandler(S3Event evnt, ILambdaContext context)
-    {
-        var destBucket = "imaging-destination";
-        var s3Event = evnt.Records?[0].S3;
-        if (s3Event == null)
+        public async Task FunctionHandler(S3Event evnt, ILambdaContext context)
         {
-            context.Logger.LogLine("No S3 event detected in the Lambda event.");
-            return "No S3 event detected in the Lambda event.";
-        }
-
-        try
-        {
-            var metadataResponse = await this.S3Client.GetObjectMetadataAsync(s3Event.Bucket.Name, s3Event.Object.Key);
-
-            if (!metadataResponse.Headers.ContentType.StartsWith("image/"))
+            try
             {
-                context.Logger.LogLine($"The file {s3Event.Object.Key} is not an image.");
-                return $"The file {s3Event.Object.Key} is not an image.";
-            }
-
-            using (GetObjectResponse response = await S3Client.GetObjectAsync(s3Event.Bucket.Name, s3Event.Object.Key))
-            using (Stream responseStream = response.ResponseStream)
-            using (var memstream = new MemoryStream())
-            {
-                await responseStream.CopyToAsync(memstream);
-                memstream.Position = 0;
-
-                byte[] thumbnailBytes = ImagingOperations.GetConvertedImage(memstream.ToArray(), 150, 150);
-                using (var thumbnailStream = new MemoryStream(thumbnailBytes))
+                foreach (var record in evnt.Records)
                 {
-                    PutObjectRequest putRequest = new PutObjectRequest
+                    var bucketName = record.S3.Bucket.Name;
+                    var key = record.S3.Object.Key;
+
+                    // Check if the event is for the original-images bucket
+                    if (bucketName != "originall-images")
                     {
-                        BucketName = destBucket,
-                        Key = $"thumbnail-{s3Event.Object.Key}",
-                        InputStream = thumbnailStream,
-                        ContentType = "image/jpeg" // Ensure the correct content type is set for the thumbnail
+                        context.Logger.LogLine($"Ignoring non-original-images bucket: {bucketName}");
+                        continue;
+                    }
+
+                    var thumbnailKey = "thumbnails/" + key; // Change the path as needed
+
+                    var getObjectRequest = new GetObjectRequest
+                    {
+                        BucketName = bucketName,
+                        Key = key
                     };
 
-                    await S3Client.PutObjectAsync(putRequest);
+                    using (var response = await _s3Client.GetObjectAsync(getObjectRequest))
+                    using (var imageStream = response.ResponseStream)
+                    {
+                        using (var thumbnail = new GcBitmap())
+                        {
+                            thumbnail.Load(imageStream);
+                            thumbnail.Resize(100, 100); // Adjust the thumbnail size as needed
+
+                            // Save thumbnail to S3
+                            using (var memoryStream = new MemoryStream())
+                            {
+                                thumbnail.SaveAsJpeg(memoryStream);
+                                memoryStream.Position = 0;
+
+                                var putObjectRequest = new PutObjectRequest
+                                {
+                                    BucketName = DestinationBucketName,
+                                    Key = thumbnailKey,
+                                    InputStream = memoryStream,
+                                    ContentType = "image/jpeg" // Change content type if needed
+                                };
+
+                                await _s3Client.PutObjectAsync(putObjectRequest);
+                            }
+                        }
+                    }
                 }
             }
-            context.Logger.LogLine($"Thumbnail created for {s3Event.Object.Key}");
-            return $"Thumbnail created for {s3Event.Object.Key}";
-        }
-        catch (Exception e)
-        {
-            context.Logger.LogLine($"Error processing {s3Event.Object.Key}: {e.Message}");
-            return $"Error processing {s3Event.Object.Key}: {e.Message}";
+            catch (Exception ex)
+            {
+                context.Logger.LogLine($"Error: {ex.Message}");
+                throw;
+            }
         }
     }
-
 }
